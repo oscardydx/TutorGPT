5260c1d526710c85fb209d0c4628fa15ca373034
Oscar Peñuela <oscar@analiza.xyz>
2025-01-29 15:46:01 +0000

    se agrega posibilidad de varios modelos y con aceleraci[on de hardware

--- chatbots/telegramAdmin_bot.py
+++ chatbots/telegramAdmin_bot.py
@@ -2,30 +2,6 @@ import logging
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 from decouple import config
-from transformers import pipeline
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
-
-import torchvision
-
-print(f"PyTorch version: {torch.__version__}")
-print(f"Torchvision version: {torchvision.__version__}")
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-#model_id = "meta-llama/Llama-3.2-3B-Instruct"
-#model_id ="Qwen/Qwen2.5-7B-Instruct"
-#model_id ="gpt2"
-
-# Cargar el tokenizador y modelo
-tokenizer = AutoTokenizer.from_pretrained(model_id,padding_side="left")
-model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", offload_folder="offload_dir", torch_dtype=torch.float16).to(device)
-
-# Asignar manualmente el pad_token_id si es necesario
-if tokenizer.pad_token is None:
-    tokenizer.pad_token = tokenizer.eos_token
-
-# Crear el pipeline usando el modelo y tokenizador cargados
-generator = pipeline("text-generation", model=model,  tokenizer=tokenizer)
 
 
 # Configuración de logging
@@ -47,35 +23,11 @@ async def start(update: Update, context):
     await update.message.reply_text("¡Hola! Soy tu bot de Telegram. Por favor, comparte tu número de teléfono.", reply_markup=reply_markup)
 
 async def echo(update: Update, context):
-
-    print("Llego mensaje.....")
     user_id = update.message.from_user.id
     user_message = update.message.text
 
-    # Iniciar una nueva conversación si es el primer mensaje
-    if user_id not in user_conversations:
-        user_conversations[user_id] = []
-
-    # Añadir el mensaje del usuario al historial de conversación
-    user_conversations[user_id].append(f"Usuario: {user_message}")
-
-    # Solo utilizar el último mensaje para generar la respuesta
-    input_text = user_conversations[user_id][-1]
-
-    # Generar una respuesta con el modelo DialoGPT
-    conversation = generator(input_text, max_length=100, truncation=True)
-
-    # Imprimir la respuesta para depuración
-    print(conversation)  # Aquí vemos cómo está estructurada la respuesta
-
-    # Acceder a la respuesta generada
-    bot_response = conversation[0]['generated_text']
-
-    # Añadir la respuesta del bot al historial de conversación
-    user_conversations[user_id].append(f"Bot: {bot_response}")
-
     # Enviar la respuesta al usuario
-    await update.message.reply_text(f" {bot_response}")
+    await update.message.reply_text(f"En Construcción: {user_message}, User id: {user_id} ")
 
 # Función para manejar el número de teléfono enviado
 async def handle_contact(update: Update, context):
@@ -105,7 +57,7 @@ def buscar_en_base_de_datos(phone_number):
 
 # Configuración del bot
 def main():
-    token = config("TOKEN_TELEGRAM")
+    token = config("TOKEN_TELEGRAMADMIN")
     app = ApplicationBuilder().token(token).build()
 
     # Comandos y manejadores
