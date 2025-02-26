32dd043781fd0309ba8e4edc7524ad533dfb14c2
Oscar Peñuela <oscar@analiza.xyz>
2025-02-26 17:03:43 +0000

    se remueve id de yandex

--- chatbots/yandex.py
+++ chatbots/yandex.py
@@ -1,18 +1,17 @@
 import requests
-
+from decouple import config
 # Configuración
-IAM_TOKEN = "t1.9euelZqeyZnKyJyKnc-Ki5adnZ2Kne3rnpWal82dyY6Sy8eSyMmdm5mNmcvl8_dONg1C-e9sd0pS_t3z9w5lCkL572x3SlL-zef1656VmsbJkc2Zy5eSiZ6KzYuNnJnO7_zF656VmsbJkc2Zy5eSiZ6KzYuNnJnO.AD3GP02VhZWsMKnYsuJKYVGKSDWg9fbm1amcUXgXWVIdKEWOLh9M1H94yOT_FHheMVq7pz2OELBE1CQgRqImBw"  # Reemplaza con tu token de autenticación de Yandex Cloud
-FOLDER_ID = "b1gc4453l5d6nl17mohu"  # ID de la carpeta en Yandex Cloud
+
 ENDPOINT = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
 
 def yandex_gpt_request(prompt, max_tokens=100):
     headers = {
-        "Authorization": f"Bearer {IAM_TOKEN}",
+        "Authorization": f"Bearer {config("IAM_TOKEN")}",
         "Content-Type": "application/json"
     }
     
     data = {
-        "modelUri": f"gpt://{FOLDER_ID}/yandexgpt/latest",
+        "modelUri": f"gpt://{config("FOLDER_ID")}/yandexgpt/latest",
         "completionOptions": {
             "stream": False,
             "temperature": 0.6,
